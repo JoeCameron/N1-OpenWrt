a9c457f3a1db68e7825e15783ea5e956ebf0c4f5
Bio <44912950+JoeCameron@users.noreply.github.com>
2025-11-03 13:49:02 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -6,10 +6,13 @@ patch -p1 -f < $(dirname "$0")/luci.patch
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/vernesong/OpenClash --depth=1 clone/OpenClash
 
 # Update packages
 rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+
+
 
 # Clean packages
 rm -rf clone
