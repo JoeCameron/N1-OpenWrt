b5009f05e18c759d99f634d74c70a0198e4a0ef6
Bio <44912950+JoeCameron@users.noreply.github.com>
2025-11-04 08:22:06 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,14 +3,20 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
+#passwall
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/luci/applications/luci-app-passwall
+
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages --depth=1 clone/passwall-packages
 git clone https://github.com/vernesong/OpenClash --depth=1 clone/OpenClash
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
 cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/passwall-packages package/passwall-packages
 
 #mosdns
 rm -rf feeds/packages/lang/golang
