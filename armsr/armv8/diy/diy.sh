eae05a5289c855f78b2214e22e82371d71ac4927
Bio <44912950+JoeCameron@users.noreply.github.com>
2025-11-04 12:42:56 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -15,8 +15,10 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages --depth=1 clone
 git clone https://github.com/vernesong/OpenClash --depth=1 clone/OpenClash
 
 # Update packages
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall clone/OpenClash/luci-app-openclash feeds/luci/applications/
-cp -rf clone/passwall-packages package/passwall-packages
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/OpenClash/luci-app-openclash feeds/luci/applications/
+cp -rf clone/passwall-packages package/
 
 #mosdns
 rm -rf feeds/packages/lang/golang
