f75c1ca9fb39d6c9f99c8ff1e346778b4e3f8009
Bio <44912950+JoeCameron@users.noreply.github.com>
2026-02-10 19:25:26 +0800

    Update diy.sh

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,14 +3,37 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+rm -rf feeds/luci/applications/luci-app-passwall
+
+
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
-git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall --depth=1 clone/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages --depth=1 clone/passwall-packages
+git clone https://github.com/vernesong/OpenClash --depth=1 clone/OpenClash
+git clone https://github.com/jerrykuku/luci-theme-argon --depth=1 clone/luci-theme-argon
 
 # Update packages
-rm -rf feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/passwall/luci-app-passwall feeds/luci/applications/
-sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/OpenClash/luci-app-openclash feeds/luci/applications/
+cp -rf clone/passwall-packages package/
+cp -rf clone/luci-theme-argon feeds/luci/applications/
+
+#mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+
+
 
 # Clean packages
 rm -rf clone
