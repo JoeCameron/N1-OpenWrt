2d4afbda8a155b08b3923bb91f373db19af3b634
cameron <snowfeiyang@gmail.com>
2026-07-08 08:45:39 +0800

    update n1

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -3,16 +3,41 @@
 # Adjust source code
 patch -p1 -f < $(dirname "$0")/luci.patch
 
-# Clone packages
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+#git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+#git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
+
+
+# Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall --depth=1 clone/passwall
-git clone https://github.com/QiuSimons/luci-app-daed clone/daed
-git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages --depth=1 clone/passwall-packages
+git clone https://github.com/vernesong/OpenClash --depth=1 clone/OpenClash
+git clone https://github.com/jerrykuku/luci-theme-argon --depth=1 clone/luci-theme-argon
 
-# Adjust packages
-rm -rf feeds/luci/applications/luci-app-daed feeds/luci/applications/luci-app-passwall
-cp -rf clone/amlogic/luci-app-amlogic clone/daed/luci-app-daed clone/passwall/luci-app-passwall feeds/luci/applications/
-sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
+# Update packages
+cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
+cp -rf clone/passwall/luci-app-passwall feeds/luci/applications/
+cp -rf clone/OpenClash/luci-app-openclash feeds/luci/applications/
+cp -rf clone/passwall-packages package/
+cp -rf clone/luci-theme-argon feeds/luci/applications/
+
+#mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+
+
 
 # Clean packages
 rm -rf clone
